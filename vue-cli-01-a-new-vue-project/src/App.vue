b509a37802d6a1ee4bc125625a94e9c7a6c001ac
choifriendship <choifriendship@naver.com>
2025-05-12 15:48:37 +0900

    Vue CLI

---
+++ vue-cli-01-a-new-vue-project/src/App.vue
@@ -0,0 +1,38 @@
+<template>
+    <section>
+        <header>
+            <h1>My Friends</h1>
+        </header>
+        <ul>
+            <friend-contact></friend-contact>
+            <friend-contact></friend-contact>
+        </ul>
+    </section>
+</template>
+
+<script>
+export default {
+    data() {
+      return {
+        friends: [
+            {
+                id: 'manuel',
+                name: 'Manuel Lorenz',
+                phone: '0123 45678 90',
+                email: 'manuel@localhost.com'
+            },
+            {
+                id: 'julie',
+                name: 'Julie Jones',
+                phone: '9876 5432 10',
+                email: 'julie@localhost.com'
+            }
+        ]
+      };
+    }
+};
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css2?family=Jost&display=swap');
+</style>
