70639b6af1bd826bf8bf20c9277b747cda4b4ef4
choifriendship <choifriendship@naver.com>
2025-05-13 17:17:27 +0900

    컴포넌트 통신

---
+++ cmp-communication-01-starting-setup/src/components/FriendContact.vue
@@ -0,0 +1,96 @@
+<template>
+  <li>
+    <h2>{{ name }} {{ isFavorite ? "(Favorite)" : "" }}</h2>
+    <button @click="toggleFavorite">Toggle Favorite</button>
+    <button @click="toggleDetails">
+      {{ detailsAreVisible ? "Hide" : "Show" }} Details
+    </button>
+    <ul v-if="detailsAreVisible">
+      <li>
+        <strong>Phone:</strong>
+        {{ phoneNumber }}
+      </li>
+      <li>
+        <strong>Email:</strong>
+        {{ emailAddress }}
+      </li>
+    </ul>
+    <button @click="$emit('delete', id)">Delete</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // props: ["name", "phoneNumber", "emailAddress", "isFavorite"],
+  props: {
+    id: {
+      type: String,
+      required: true,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    emailAddress: {
+      type: String,
+      required: true,
+    },
+    isFavorite: {
+      type: Boolean,
+      required: false,
+      default: false,
+      // validator: function(value) {
+      //   return value === '1' || value === '0';
+      // }
+    },
+  },
+  emits: ['toggle-favorite'],
+  // emits: {
+  //   "toggle-favorite": function (id) {
+  //     if (id) {
+  //       return true;
+  //     } else {
+  //       console.warn("Id is missing!");
+  //       return false;
+  //     }
+  //   },
+  // },
+  data() {
+    return {
+      detailsAreVisible: false,
+      // friend: {
+      //   id: "manuel",
+      //   name: "Manuel Lorenz",
+      //   phone: "0123 45678 90",
+      //   email: "manuel@localhost.com",
+      // },
+      // friendIsFavorite: this.isFavorite,
+    };
+  },
+  methods: {
+    toggleDetails() {
+      this.detailsAreVisible = !this.detailsAreVisible;
+    },
+    toggleFavorite() {
+      // if (this.friendIsFavorite === '1') {
+      //   this.friendIsFavorite = '0';
+      // } else {
+      //   this.friendIsFavorite = '1';
+      // }
+
+      // this.friendIsFavorite = !this.friendIsFavorite;
+
+      // 부모 메서드로 수신할 수 있는 커스텀 이벤트를 발생시켜준다.
+      // 이벤트는 케밥 케이스만 사용
+      this.$emit("toggle-favorite", this.id);
+    },
+    // deleteFriend() {
+    //   this.$emit('delete');
+    // }
+  },
+};
+</script>
