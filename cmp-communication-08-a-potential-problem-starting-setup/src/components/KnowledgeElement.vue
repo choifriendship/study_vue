ff1f04c9bb221bcbada6551f6b645463314d507f
choifriendship <choifriendship@naver.com>
2025-05-14 16:56:16 +0900

    provide와 inject

---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeElement.vue
@@ -0,0 +1,20 @@
+<template>
+  <li>
+    <h3>{{ topicName }}</h3>
+    <p>{{ description }}</p>
+    <button @click="selectTopic(id)">Learn More</button>
+  </li>
+</template>
+
+<script>
+export default {
+  // provide와 inject는 부모 자식 관계에서만 사용 가능
+  // 이웃 관계에서 사용 불가능
+  // 여러 컴포넌트에서 사용되어야 하는 데이터인 경우 사용
+  inject: ['selectTopic'],
+  // props는 프로퍼티를 사용해서 데이터를 부모 -> 자식으로 단방향 전송
+  props: ['id', 'topicName', 'description'],
+  // emits은 자식 => 부모로 전송하는 커스텀 이벤트
+  emits: ['select-topic'],
+};
+</script>
---
+++ cmp-communication-08-a-potential-problem-starting-setup/src/components/KnowledgeGrid.vue
@@ -0,0 +1,20 @@
+<template>
+  <ul>
+    <knowledge-element
+      v-for="topic in topics"
+      :key="topic.id"
+      :id="topic.id"
+      :topic-name="topic.title"
+      :description="topic.description"
+    ></knowledge-element>
+  </ul>
+</template>
+
+<script>
+export default {
+  // props: ['topics'],
+  // 상위 레벨에서 제공된 것만 참조할 수 있다. provide - inject 부모 자식 관계
+  // 부모 컴포넌트나 조상 컴포넌트 (현재는 App.vue)
+  inject: ['topics'],
+};
+</script>
