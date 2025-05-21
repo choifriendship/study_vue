1c5e85560e1b192a0d233e33d2de918798a8d9ba
choifriendship <choifriendship@naver.com>
2025-05-21 10:41:17 +0900

    양식(forms)

---
+++ forms-01-starting-setup/src/components/RatingControl.vue
@@ -0,0 +1,70 @@
+<template>
+  <ul>
+    <li :class="{ active: modelValue === 'poor' }">
+      <button type="button" @click="activate('poor')">Poor</button>
+    </li>
+    <li :class="{ active: modelValue === 'average' }">
+      <button type="button" @click="activate('average')">Average</button>
+    </li>
+    <li :class="{ active: modelValue === 'great' }">
+      <button type="button" @click="activate('great')">Great</button>
+    </li>
+  </ul>
+</template>
+
+<script>
+export default {
+  // 부모 → 자식 컴포넌트로 데이터를 전달할 때 사용
+  props: ['modelValue'],
+  // 자식 → 부모 컴포넌트로 이벤트를 전달할 때 사용합니다.
+  emits: ['update:modelValue'],
+  //   data() {
+  //     return {
+  //       activeOption: this.modelValue,
+  //     };
+  //   },
+  computed: {
+    activeOption() {
+      return this.modelValue;
+    },
+  },
+  methods: {
+    activate(option) {
+      this.$emit('update:modelValue', option);
+    },
+  },
+};
+</script>
+
+<style scoped>
+.active {
+  border-color: #a00078;
+}
+
+.active button {
+  color: #a00078;
+}
+
+ul {
+  list-style: none;
+  margin: 0.5rem 0;
+  padding: 0;
+  display: flex;
+}
+
+li {
+  margin: 0 1rem;
+  border: 1px solid #ccc;
+  padding: 1rem;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+button {
+  font: inherit;
+  border: none;
+  background-color: transparent;
+  cursor: pointer;
+}
+</style>
