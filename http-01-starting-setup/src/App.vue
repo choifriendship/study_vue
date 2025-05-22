e6cc6147c0469a28c7b139ecf3d0079bd43836b5
choifriendship <choifriendship@naver.com>
2025-05-22 11:25:51 +0900

    HTTP 요청 보내기

---
+++ http-01-starting-setup/src/App.vue
@@ -0,0 +1,51 @@
+<template>
+  <learning-survey @survey-submitted="handleSurveySubmitted"></learning-survey>
+  <user-experiences ref="userExperiences"></user-experiences>
+</template>
+
+<script>
+import LearningSurvey from './components/survey/LearningSurvey.vue';
+import UserExperiences from './components/survey/UserExperiences.vue';
+
+export default {
+  components: {
+    LearningSurvey,
+    UserExperiences,
+  },
+  methods: {
+    handleSurveySubmitted() {
+      this.$refs.userExperiences.loadExperiences();
+    }
+  }
+  // data() {
+  //   return {
+  //     savedSurveyResults: [],
+  //   };
+  // },
+  // methods: {
+  //   storeSurvey(surveyData) {
+  //     const surveyResult = {
+  //       name: surveyData.userName,
+  //       rating: surveyData.rating,
+  //       id: new Date().toISOString(),
+  //     };
+  //     this.savedSurveyResults.push(surveyResult);
+  //     console.log(surveyResult);
+  //   },
+  // },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: sans-serif;
+}
+
+body {
+  margin: 0;
+}
+</style>
