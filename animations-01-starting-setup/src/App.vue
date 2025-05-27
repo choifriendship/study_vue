a1855ac96578bdf52983e8e00d7389f86b411777
choifriendship <choifriendship@naver.com>
2025-05-27 13:42:25 +0900

    transition-group, route 애니메이션

--- animations-01-starting-setup/src/App.vue
+++ animations-01-starting-setup/src/App.vue
@@ -1,18 +1,23 @@
 <template>
+  <!-- <div class="container">
+    <users-list></users-list>
+  </div>
   <div class="container">
     <div class="block" :class="{ animate: animatedBlock }"></div>
     <button @click="animateBlock">Animate</button>
   </div>
-  <div class="container">
-    <!-- enter-to-class="some-class" enter-active-class="..." -->
-    <transition
-      name="para"
+  <div class="container"> -->
+  <!-- <enter-to-class="some-class" enter-active-class="..."> -->
+  <!-- <transition
+      :css="false"
       @before-enter="beforeEnter"
       @enter="enter"
       @after-enter="afterEnter"
       @before-leave="beforeLeave"
       @leave="leave"
       @after-leave="afterLeave"
+      @enter-cancelled="enterCancelled"
+      @leave-cancelled="leaveCancelled"
     >
       <p v-if="paraIsVisible">This is only sometimes visible...</p>
     </transition>
@@ -30,17 +35,29 @@
   </base-modal>
   <div class="container">
     <button @click="showDialog">Show Dialog</button>
-  </div>
-</template>  
+  </div> -->
+  <router-view v-slot="slotProps">
+    <transition name="fade-button" mode="out-in">
+      <component :is="slotProps.Component"></component>
+    </transition>
+  </router-view>
+</template>
 
 <script>
+// import UsersList from './components/UserList.vue';
+
 export default {
+  components: {
+    // UsersList
+  },
   data() {
     return {
       dialogIsVisible: false,
       animatedBlock: false,
       paraIsVisible: false,
       usersAreVisible: false,
+      enterInterval: null,
+      leaveInterval: null,
     };
   },
   methods: {
@@ -65,26 +82,54 @@ export default {
     beforeEnter(el) {
       console.log('beforEnter');
       console.log(el);
+      el.style.opacity = 0;
     },
     beforeLeave(el) {
       console.log('beforeLeave');
       console.log(el);
+      el.style.opacity = 1;
     },
-    enter(el) {
+    enter(el, done) {
       console.log('enter');
       console.log(el);
+      let round = 1;
+      this.enterInterval = setInterval(() => {
+        el.style.opacity = round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.enterInterval);
+          done();
+        }
+      }, 20);
     },
     afterEnter(el) {
       console.log('afterEnter');
       console.log(el);
     },
-    leave(el) {
+    leave(el, done) {
       console.log('leave');
       console.log(el);
+      let round = 1;
+      this.leaveInterval = setInterval(() => {
+        el.style.opacity = 1 - round * 0.01;
+        round++;
+        if (round > 100) {
+          clearInterval(this.leaveInterval);
+          done();
+        }
+      }, 20);
     },
     afterLeave(el) {
       console.log('afterLeave');
       console.log(el);
+    },
+    enterCancelled(el) {
+      console.log(el);
+      clearInterval(this.enterInterval);
+    },
+    leaveCancelled(el) {
+      console.log(el);
+      clearInterval(this.leaveInterval);
     },
   },
 };
@@ -137,35 +182,35 @@ button:active {
   animation: slide-fade 0.3s ease-out forwards;
 }
 
-.para-enter-from {
-  /* opacity: 0;
-  transform: translateY(-30px); */
-}
+/* .para-enter-from {
+  opacity: 0;
+  transform: translateY(-30px);
+} */
 
-.para-enter-active {
+/* .para-enter-active {
   animation: slide-scale 2s ease-out;
-  /* transition: all 0.3s ease-out; */
-}
+  transition: all 0.3s ease-out;
+} */
 
-.para-enter-to {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
+/* .para-enter-to {
+  opacity: 1;
+  transform: translateY(0);
+} */
 
-.para-leave-from {
-  /* opacity: 1;
-  transform: translateY(0); */
-}
+/* .para-leave-from {
+  opacity: 1;
+  transform: translateY(0);
+} */
 
-.para-leave-active {
-  /* transition: all 0.3s ease-in; */
+/* .para-leave-active {
+  transition: all 0.3s ease-in;
   animation: slide-scale 0.3s ease-out;
-}
+} */
 
-.para-leave-to {
-  /* opacity: 0;
-  transform: translateY(30px); */
-}
+/* .para-leave-to {
+  opacity: 0;
+  transform: translateY(30px);
+} */
 
 .fade-button-enter-from,
 .fade-button-leave-to {
@@ -183,6 +228,14 @@ button:active {
 .fade-button-enter-to,
 .fade-button-leave-from {
   opacity: 1;
+}
+
+.route-enter-active {
+  animation: slide-scale 0.4s ease-out;
+}
+
+.route-leave-active {
+  animation: slide-scale 0.4s ease-in;
 }
 
 @keyframes slide-scale {
