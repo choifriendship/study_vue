0df4c9bef2e70c3e7d419fddbbf0d3dd46c91fc2
choifriendship <choifriendship@naver.com>
2025-05-28 15:57:52 +0900

    Vuex Mutation

---
+++ vuex-01-starting-setup/src/components/ChangeCounter.vue
@@ -0,0 +1,14 @@
+<template>
+  <button @click="addOne">Add 1</button>
+</template>
+
+<script>
+export default {
+  methods: {
+    addOne() {
+      // main.js에 있는 mutations에 있는 이름을 가져와야 함
+      this.$store.commit('increment');
+    },
+  },
+};
+</script>
