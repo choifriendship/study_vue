ce0f5f06ae295c5f6f05e790ddf379d3ca7d2221
choifriendship <choifriendship@naver.com>
2025-05-30 17:15:04 +0900

    vuex 과제

--- vuex-01-starting-setup/src/store/actions.js
+++ vuex-01-starting-setup/src/store/actions.js
@@ -1,8 +1,8 @@
 export default {
-            login(context) {
-            context.commit('setAuth', { isAuth: true });
-        },
-        logout(context) {
-            context.commit('setAuth', { isAuth: false });
-        }
+    login(context) {
+        context.commit('setAuth', { isAuth: true });
+    },
+    logout(context) {
+        context.commit('setAuth', { isAuth: false });
+    }
 }
---
+++ vuex-11-a-challenge-starting-code/.eslintrc.js
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  parser: 'vue-eslint-parser',
+  parserOptions: {
+    parser: '@babel/eslint-parser',
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    requireConfigFile: false,
+  },
+  extends: [
+    'plugin:vue/vue3-essential',
+    'eslint:recommended',
+  ],
+  rules: {
+    'no-console': 'off',
+    'no-debugger': 'off',
+  },
+};
