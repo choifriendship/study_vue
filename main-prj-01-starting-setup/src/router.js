60173cc22f489895683058b76dbadfe3aabc33e8
choifriendship <choifriendship@naver.com>
2025-06-12 17:34:34 +0900

    비동기 라우팅 수정

--- main-prj-01-starting-setup/src/router.js
+++ main-prj-01-starting-setup/src/router.js
@@ -1,4 +1,3 @@
-import { defineAsyncComponent } from 'vue';
 import { createRouter, createWebHistory } from 'vue-router';
 
 import CoachesList from './pages/coaches/CoachesList.vue';
@@ -10,21 +9,11 @@ import NotFound from './pages/NotFound.vue';
 // import UserAuth from './pages/auth/UserAuth.vue';
 import store from './pages/store/index.js';
 
-const CoachDetail = defineAsyncComponent(() =>
-  import('./pages/coaches/CoachesList.vue')
-);
-const CoachResistration = defineAsyncComponent(() =>
-  import('./pages/coaches/CoachResistration.vue')
-);
-const ContactCoach = defineAsyncComponent(() =>
-  import('./pages/requests/ContactCoach.vue')
-);
-const RequestsReceived = defineAsyncComponent(() =>
-  import('./pages/requests/RequestsReceived.vue')
-);
-const UserAuth = defineAsyncComponent(() =>
-  import('./pages/auth/UserAuth.vue')
-);
+const CoachDetail = () => import('./pages/coaches/CoachesList.vue');
+const CoachResistration = () => import('./pages/coaches/CoachResistration.vue');
+const ContactCoach = () => import('./pages/requests/ContactCoach.vue');
+const RequestsReceived = () => import('./pages/requests/RequestsReceived.vue');
+const UserAuth = () => import('./pages/auth/UserAuth.vue');
 
 const router = createRouter({
   history: createWebHistory(),
