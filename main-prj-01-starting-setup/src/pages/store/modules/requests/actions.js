cfed68a280e70af234dda117c4c5601979959b52
choifriendship <choifriendship@naver.com>
2025-06-12 14:35:12 +0900

    인증 및 라우팅, 자동 로그인·로그아웃 추가

--- main-prj-01-starting-setup/src/pages/store/modules/requests/actions.js
+++ main-prj-01-starting-setup/src/pages/store/modules/requests/actions.js
@@ -30,8 +30,9 @@ export default {
   },
   async fecthRequests(context) {
     const coachId = context.rootGetters.userId;
+    const token = context.rootGetters.token;
     const response = await fetch(
-      `https://vue-http-demo-9deed-default-rtdb.firebaseio.com/requests/${coachId}.json`
+      `https://vue-http-demo-9deed-default-rtdb.firebaseio.com/requests/${coachId}.json?auth=` + token
     );
     const responseData = await response.json();
 
