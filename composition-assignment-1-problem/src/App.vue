2ab6dcd975c46b72c3157f3387dba9936b82cc3f
choifriendship <choifriendship@naver.com>
2025-06-16 15:00:46 +0900

    컴포지션 API 사용한 과제

---
+++ composition-assignment-1-problem/src/App.vue
@@ -0,0 +1,35 @@
+<template>
+  <h2>My Course Goal</h2>
+  <h3 v-if="showGoal">{{ reactiveGoal.goal }}</h3>
+  <button @click="toggleGoal">Toggle Goal</button>
+</template>
+
+<script>
+import { ref, reactive } from 'vue';
+export default {
+  setup() {
+    // const refGoal = ref('Finish vue Study with ref');
+    const reactiveGoal = reactive({
+      goal: 'Finish Vue Study! with reactive'
+    });
+
+    const showGoal = ref(true);
+
+    function toggleGoal() {
+      showGoal.value = !showGoal.value;
+    }
+
+    return { reactiveGoal, toggleGoal, showGoal };
+  },
+};
+</script>
+
+<style>
+html {
+  font-family: sans-serif;
+}
+body {
+  margin: 3rem;
+  text-align: center;
+}
+</style>
