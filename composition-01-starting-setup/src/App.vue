f9bdd7b057f6f4239e321b3365ab07df5519f2ac
choifriendship <choifriendship@naver.com>
2025-06-16 15:36:23 +0900

    양방향 바인딩, computed, watcher

--- composition-01-starting-setup/src/App.vue
+++ composition-01-starting-setup/src/App.vue
@@ -1,45 +1,57 @@
 <template>
   <section class="container">
-    <h2>{{ user.name }}</h2>
-    <h3>{{ user.age }}</h3>
+    <h2>{{ userName }}</h2>
+    <h3>{{ uAge }}</h3>
     <button @click="setAge">Change Age</button>
+    <div>
+      <!-- <input type="text" placeholder="First Name" @input="setFirstName" />
+      <input type="text" placeholder="Last Name" @input="setLastName" /> -->
+      <input type="text" placeholder="First Name" v-model="firstName" />
+      <input type="text" placeholder="Last Name" v-model="lastName" />
+    </div>
   </section>
 </template>
 
 <script>
 // 반응형 데이터 reactive
 // ref: 객체를 포함해 다양한 값을 가질 수 있음
-import { reactive } from 'vue';
+import { ref, computed, watch } from 'vue';
 
 export default {
   setup() {
-    // const uName = ref('woojeongchoi');
-    // const uAge = ref(31);
-    const user = reactive({
-      name: 'woojeongchoi',
-      age: 28,
+    const firstName = ref('');
+    const lastName = ref('');
+    const uAge = ref(28);
+    // const user = reactive({
+    //   name: 'woojeongchoi',
+    //   age: 28,
+    // });
+
+    const uName = computed(function () {
+      return firstName.value + ' ' + lastName.value;
+    });
+
+    // 감시 함수
+    watch([uAge, uName], function (newValues, oldValues) {
+      console.log('Old age: ' + oldValues[0]);
+      console.log('New age: ' + newValues[0]);
+      console.log('Old name: ' + oldValues[1]);
+      console.log('New name: ' + newValues[1]);
     });
 
     function setNewAge() {
-      user.age = 21;
+      uAge.value = 21;
     }
 
-    // setTimeout(function () {
-    //   // uName.value = 'friendship';
-    //   // uAge.value = 21;
-    //   // user.value.name = 'friendship';
-    //   // user.value.age = 21;
-    //   user.name = 'friendship';
-    //   user.age = 21;
-    // }, 2000);
+    // function setFirstName(event) {
+    //   firstName.value = event.target.value;
+    // }
+    // function setLastName(event) {
+    //   lastName.value = event.target.value;
+    // }
 
-    return { user: user, setAge: setNewAge };
+    return { uAge, userName: uName, setAge: setNewAge, firstName, lastName };
   },
-  // data() {
-  //   return {
-  //     userName: 'Maximilian',
-  //   };
-  // },
 };
 </script>
 
